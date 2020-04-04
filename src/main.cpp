01a0c5eb90053e9e2c621852802fb84f2221eee9
ddwooten <danieldavidwooten@gmail.com>
2020-04-03 18:56:54 -0700

    Added docs to main

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,24 @@
 
 using namespace std;
 
+/** Executing function of the programing challenge.
+  * main() may optionally take 1 command line argument. This argument is the
+  * relative or absolute path to an input file for the programing challenge.
+  * main() uses a default value for this path of './../input.txt' which will
+  * be overridden by *any* command line input following the invocation of
+  * the executable.
+  * @param argc The number of command line arguments including the function name
+  * @param argv Array of char* to command line inputs in string format
+  */
+
 int main(int argc, char** argv)
 {
 
-	char file_name[256];
+	char file_name[256]; /** Memory for the file name */
 
-	fstream in_file;
+	fstream in_file; /** Streaming file object used here for input */
 
-	trick_or_treat* hopes_and_dreams;
+	trick_or_treat* hopes_and_dreams; /** @see trick_or_treat */
 
 	/* If no arguments give, use default run settings */
 
