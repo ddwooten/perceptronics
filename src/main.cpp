e144f6a80281ecfda545f1c07142795aa8879f0f
ddwooten <danieldavidwooten@gmail.com>
2020-07-06 08:31:16 -0700

    Updated main for this project

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 #include <iostream>
 #include <fstream>
 #include <omp.h>
-#include "trick_or_treat.h"
+#include "nilakantha_series.h"
 
 using namespace std;
 
 /** 
   * Executing function of the programming challenge.
-  * main() may optionally take 1 command line argument. This argument is the
+  * main() ay optionally take 1 command line argument. This argument is the
   * relative or absolute path to an input file for the programming challenge.
   * main() uses a default value for this path of './../input.txt' which will
   * be overridden by *any* command line input following the invocation of
@@ -20,46 +20,60 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+	
+	/** Storage for user input before integer conversion */
+	char input[128];
 
-	/** Memory for the file name. */
-	char file_name[256];
+	/** Integer user input for desired number of terms in computation */
+	int terms = 0;
 
-	/** Streaming file object used here for input. */
-	fstream in_file;
+	/** @see nilakantha_series. */
+	nilakantha_series* this_series;
 
-	/** @see trick_or_treat. */
-	trick_or_treat* hopes_and_dreams;
-
-	/* If no arguments give, use default run settings */
+	/* If no arguments given, error out */
 
 	if(argc < 2)
 	{
 
-		strcpy(file_name, "./../input.txt");
+		fprintf(stderr, "No input given with program invocation. Usage of the Nilakantha calculator requires the inclusion of one command line argument being a positive integer. E.g. any of 1, 2, 3...\n");
+
+		return(0);
 
 	}
 	else
 	{
 
-		strcpy(file_name, argv[1]);
+		/* Otherwise take in a protected slice of user input and
+		   attempt to convert to integer */
 
+		strncpy(input, argv[2], 127);
+
+		input[127] = "\0"
+
+		terms = atoi(input);
+
+		/* If the conversion failed, error out with message */
+		
+		if(terms == 0)
+		{
+
+			fprintf(stderr, "Attempted conversion of user input, %s, to integer. Conversion failed. Please invoke this program with command line input which can be converted from type of string to type of integer.\n", input);
+
+			return(0);
+
+		}
+		
 	}
 
-	/* Initiate the trick_or_treat class into a child's hope's and dreams */
+	/* Initiate the nilakantha_series class */
 
-	hopes_and_dreams = new trick_or_treat{};
+	this_series = new nilakantha_series{};
 
-	/* Read in the input */
+	/* Begin the calculation */
 
-	hopes_and_dreams -> read_input(file_name);
+	this_series -> calculate(terms);
 
-	/* Find the optimal candy route */
-
-	hopes_and_dreams -> find_path();
-
-	/* Print out the result */
-
-	hopes_and_dreams -> report();
+	/* Exit gracefully */
 
 	return(1);
 
